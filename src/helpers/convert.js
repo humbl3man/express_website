24167db6c0d2aad8058730cd8771a678777e4e26
Sstan Minevich <imhumbl3@gmail.com>
2016-08-19 12:53:12 -0700

    refactor convert js

--- src/helpers/convert.js
+++ src/helpers/convert.js
@@ -1,7 +1,19 @@
 function objToArray(obj) {
-	return Object.keys(obj).map(function(key) {
-		return obj[key];
-	});
+	'use strict';
+	try {
+		if (obj && typeof obj === 'object') {
+			return Object.keys(obj).map(function(key) {
+				return obj[key];
+			});
+		} else {
+			throw new Error({
+				name: 'Not Object',
+				description: 'This is not an object!'
+			});
+		}
+	} catch (error) {
+		console.error(error.name, error.description);
+	}
 }
 
 module.exports = {
